c9128f154d7328c1bd55d1325a891eec90990727
hgregorian <theheig@gmail.com>
2016-11-26 21:38:18 -0800

    Bump minor version since there's too much going on here

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'theheig@gmail.com'
 license 'all_rights'
 description 'Installs/Configures hg_automount_bays'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.1'
+version '0.3.0'
 depends 'logrotate', '~> 1.9'
 depends 'systemd', '~> 2.1.2'
 depends 'hg_sas3ircu'
